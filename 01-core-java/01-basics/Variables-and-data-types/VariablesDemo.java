217a2b7fdc19b753814706a2df3e1c8f440742f5
kusumapachar2004-coder <kusumapachar2004@gmail.com>
2026-08-25 23:58:03 +0530

    Create VariablesDemo.java

---
+++ 01-core-java/01-basics/Variables-and-data-types/VariablesDemo.java
@@ -0,0 +1,29 @@
+A variable is a named memory location used to store data that can change during program execution.
+  Declaration: You must specify the type and name
+  example: int age;
+
+Initialization: Assign a value when declaring or later.
+  example:
+int age = 22;
+
+DATA TYPES
+  
+1. Primitive Data Types (built-in, simple values)
+byte → 8-bit integer (range: -128 to 127)
+short → 16-bit integer
+int → 32-bit integer (commonly used)
+long → 64-bit integer
+float → 32-bit decimal (less precise)
+double → 64-bit decimal (more precise)
+char → single character (e.g., 'A')
+boolean → true/false
+
+2. Non-Primitive (Reference) Data Types
+String → sequence of characters (e.g., "Kusuma")
+Arrays → collection of similar data types
+Classes/Objects → user-defined types
+
+  DECLARATION AND INITIALIZATION
+  int age;          // declaration
+age = 22;         // initialization
+String name = "Kusuma"; // declaration + initialization
